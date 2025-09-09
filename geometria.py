fbfde8c13a4fc20f4cd1851d555d4a798cee28eb
joaofernandoz <joaofernandooliveirazacarias@gmail.com>
2025-09-09 08:43:18 -0300

    Commit Inicial, incluindo função "classificar_triangulo"

---
+++ geometria.py
@@ -0,0 +1,16 @@
+def classificar_triangulo(a: int|float, b: int|float, c: int|float) -> str:
+    if not all(isinstance(valor, (int, float)) for valor in (a,b,c)):
+        raise TypeError("Lados devem ser valores numéricos")
+    elif not all(valor > 0 for valor in (a,b,c)):
+        raise TypeError("Lados devem ser positivos")
+    elif not (
+        a + b > c and
+        a + c > b and
+        c + b > a): ValueError("Lados não formam um triângulo")
+
+    if a == b and b == c:
+        return "Equilátero"
+    elif (a == b and not b == c) or (b == c and not b == a):
+        return "Isósceles"
+    else:
+        return "Escaleno"
